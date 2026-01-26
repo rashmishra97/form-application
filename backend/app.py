e9f95c5be50611c0e64e137cf2221e0bc6259426
Rashmi Mishra <rashmimishra97@gmail.com>
2026-01-26 11:46:14 +0530

    port change

--- backend/app.py
+++ backend/app.py
@@ -29,4 +29,4 @@ def get_users():
     return jsonify(users), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
